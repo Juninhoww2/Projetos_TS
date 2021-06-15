c74bf384018f512605a35c5e67cdaa0333a9ebe2
alqadiano96 <alqadianoKira@gmail.com>
2021-06-15 12:53:34 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import "./styles.css";
+
+/*document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  We use the same configuration as Parcel to bundle this sandbox, you can find more
+  info about Parcel 
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
+*/
+
+const closedFace = document.querySelector(".closed");
+const openFace = document.querySelector(".open");
+
+closedFace.addEventListener("click", () => {
+  if (openFace.classList.contains("open")) {
+    openFace.classList.add("active");
+    closedFace.classList.remove("active");
+  }
+});
+
+openFace.addEventListener("click", () => {
+  if (closedFace.classList.contains("closed")) {
+    closedFace.classList.add("active");
+    openFace.classList.remove("active");
+  }
+});
